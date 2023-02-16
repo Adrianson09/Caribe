ccb0d5e75b0aa05155041f9f2793ccd39195d19c
Adrian Guerrero <adrianson@outlook.com>
2023-02-16 12:58:26 -0600

    First commit footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='bg-black/95 text-white  '>
-        <section className='flex justify-center p-10  '>
-            <div className='max-w-[650px] mr-10'>
+        <section className='max-w-[80%] m-auto grid md:grid-cols-2 sm:grid-cols-1 gap-10 p-10  '>
+            <div className='max-w-[650px] '>
                 <h3 className='font-bold'>Costa Rica</h3>
                 <a>Tel: +(506) 2208-8890</a>
                 <p className='max-w-[450px]'>205 Avenida Escazú, Piso 5, Suite 5,
                     10203 San José, Escazú,
                     San José, Costa Rica</p>
             </div>
-            <div className='max-w-[650px] ml-10'>
+            <div className='max-w-[650px] '>
                 <h3 className='font-bold'>México</h3>
                 <a>Tel: +52 (55)46313338</a>
                 <p className='max-w-[450px]'>Av. Del Imán 151, Int A110, Piso 1,
